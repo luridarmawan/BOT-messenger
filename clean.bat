4a1646dcf057cc0f3aa62104b2fa07d62fb0c405
Luri Darmawan <luri@kioss.com>
2018-03-15 01:46:57 +0700

    init file, public_html and telegram source

---
+++ clean.bat
@@ -0,0 +1,25 @@
+@echo off
+del *~
+rem del *.rst
+rem del *.lrt
+rem del *.lps
+del /s *.or
+del /s *.bak
+del /s *.exe
+del /s *.ppu
+del /s *.o
+del *.compiled
+del /s /q lib\*
+rmdir /s /q lib\
+rmdir /s /q backup\
+rmdir /s /q source\backup
+rmdir /s /q source\facebook\backup
+rmdir /s /q source\telegram\backup
+rmdir /s /q source\line\backup
+
+del /s /q source\facebook\lib\*
+del /s /q source\telegram\lib\*
+
+del /s /q public_html\carik\*.bin
+
+timeout /t 3
